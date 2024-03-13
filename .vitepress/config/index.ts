036b9099e2e67ea3271395ea2f15a2ce7cb49714
Mt-Youya <dd257248@gmail.com>
2024-03-13 14:47:10 +0800

    fix: rewrites config

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -7,7 +7,7 @@ import handleRewrites from "../utils/rewrites"
 const en = { lang: "en-us", list: EN_side() }
 const zh = { lang: "zh-cn", list: ZH_side() }
 const rewrites = handleRewrites(en, zh)
-console.log(rewrites)
+// console.log(rewrites)
 
 export default defineConfig({
     ...shared,
@@ -15,5 +15,5 @@ export default defineConfig({
         "en-us": { label: "English", ...en_us },
         root: { label: "简体中文", ...zh_cn },
     },
-    rewrites,
+    // rewrites,
 })
